817a388a57cd111a4524ad6c3f449e5c782ed95c
575 <i0850710@gmail.com>
2022-09-10 01:05:58 +0800

    [type-challenges/medium] 2-get-return-type finished

---
+++ type-challenges/_speed-run/medium.ts
@@ -0,0 +1,24 @@
+import type { Equal, Expect, NotAny } from "@type-challenges/utils";
+
+/* _____________ 2 - 获取函数返回类型 你的代码 _____________ */
+type MyReturnType<T> = any;
+
+/* _____________ 2 - 获取函数返回类型 测试用例 _____________ */
+type cases2 = [
+  Expect<Equal<string, MyReturnType<() => string>>>,
+  Expect<Equal<123, MyReturnType<() => 123>>>,
+  Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+  Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+  Expect<Equal<() => "foo", MyReturnType<() => () => "foo">>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>
+];
+
+type ComplexObject = {
+  a: [12, "foo"];
+  bar: "hello";
+  prev(): number;
+};
+
+const fn = (v: boolean) => (v ? 1 : 2);
+const fn1 = (v: boolean, w: any) => (v ? 1 : 2);
